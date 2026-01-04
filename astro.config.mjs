abda3e20f1d3cbfa06dd7aba5ef50c7416fa26d4
Bartek Jasinski <bartoszjasinski01@gmail.com>
2026-01-04 17:29:04 +0100

    Enable API routes (Astro hybrid on Vercel)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
+
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
